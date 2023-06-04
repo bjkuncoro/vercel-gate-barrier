e1acf31e837b4c3eb173d3ef5941c24b25810893
Bagus Jati Kuncoro <bagus@karyakarsa.com>
2023-06-04 16:42:50 +0700

    solve stream promblem by update package json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 
 import Nav from './nav';
-import AnalyticsWrapper from './analytics';
+// import AnalyticsWrapper from './analytics';
 import { Suspense } from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { Analytics } from '@vercel/analytics/react';
@@ -9,7 +9,7 @@ import Toast from './toast';
 // import Navbar from './navbar';
 
 export const metadata = {
-  title: 'Next.js 13 + PlanetScale + NextAuth + Tailwind CSS',
+  title: 'Gate Barrier Automation',
   description:
     'A user admin dashboard configured with Next.js, PlanetScale, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
 };
@@ -25,10 +25,10 @@ export default async function RootLayout({
     <html lang="en" className="h-full bg-gray-50">
       <body className="h-full">
         <AuthProvider>
-          {/* <Suspense fallback={<p>loading. . .</p>}> */}
+          <Suspense fallback={<p>loading. . .</p>}>
             {/* @ts-expect-error Server Component */}
             <Nav/>
-          {/* </Suspense> */}
+          </Suspense>
           {children}
         </AuthProvider>
         <Analytics />
